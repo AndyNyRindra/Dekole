6ff1f3b26e9379dfb391ae4c071f8cf71b58d8fe
AndyNyRindra <92746662+AndyNyRindra@users.noreply.github.com>
2023-01-27 19:48:11 +0300

    init backend project

---
+++ dekole_backend/src/main/java/avion/dekole/DekoleBackendApplication.java
@@ -0,0 +1,13 @@
+package avion.dekole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DekoleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DekoleBackendApplication.class, args);
+    }
+
+}
---
+++ dekole_backend/src/test/java/avion/dekole/DekoleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package avion.dekole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DekoleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
